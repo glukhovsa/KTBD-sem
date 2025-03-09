22a5cba1536fe2459b8aeade72208bab81d5c625
glukhovsa <stepa.gl@yandex.ru>
2025-03-09 22:37:48 +0300

    begining of seminar-6

---
+++ seminar-6/create-tables.sql
@@ -0,0 +1,54 @@
+CREATE TABLE TPLG_PCB_Library
+( 
+    PCBLib_ID               NUMBER      NOT NULL,
+    PCBLib_Name             CHAR(40)    UNIQUE, 
+    PCBLib_Footprint_Count  INTEGER     NOT NULL, 
+    PCBLib_YGOLib_Name      CHAR(40)    UNIQUE
+);
+
+ALTER TABLE TPLG_PCB_Library
+    ADD CONSTRAINT C_PCBLib_PK PRIMARY KEY ( PCBLib_ID );
+ALTER TABLE TPLG_PCB_Library
+    ADD CONSTRAINT C_PCBLib_Footprint_Count_CHK CHECK ( PCBLib_Footprint_Count > 0 );
+CREATE SEQUENCE S_TPLG_PCB_Library INCREMENT BY 1 START WITH 1;
+
+
+CREATE TABLE TPLG_Footprint 
+( 
+    Footprint_PCBLib_ID   NUMBER    NOT NULL,
+    Footprint_ID          NUMBER    NOT NULL,
+    Footprint_Name        CHAR(20)  UNIQUE, 
+    Footprint_Length      INTEGER   NOT NULL, 
+    Footprint_Width       INTEGER   NOT NULL,
+    Footprint_Pad_Count   NUMBER    NOT NULL, 
+    Footprint_Instalation CHAR(20)  NOT NULL, 
+    Footprint_YGO         CHAR(20)  NULL
+);
+
+ALTER TABLE TPLG_Footprint
+    ADD CONSTRAINT C_Footprint_PK PRIMARY KEY (Footprint_ID);
+ALTER TABLE TPLG_Footprint
+    ADD CONSTRAINT C_Footprint_PCBLib_FK FOREIGN KEY (Footprint_PCBLib_ID) REFERENCES TPLG_PCB_Library(PCBLib_ID);
+CREATE SEQUENCE S_TPLG_Footprint INCREMENT BY 1 START WITH 1;
+
+
+CREATE TABLE TPLG_Pad 
+( 
+    Pad_PCBLib_ID     NUMBER    NOT NULL, 
+    Pad_Footprint_ID  NUMBER    NOT NULL, 
+    Pad_ID            NUMBER    NOT NULL, 
+    Pad_Length        INTEGER   NOT NULL, 
+    Pad_Width         INTEGER   NOT NULL, 
+    Pad_Square        CHAR(20)  NOT NULL, 
+    Pad_Hole_D        INTEGER   NULL, 
+    Pad_X             INTEGER   NOT NULL, 
+    Pad_Y             INTEGER   NOT NULL
+);
+
+ALTER TABLE TPLG_Pad
+    ADD CONSTRAINT C_Pad_PK PRIMARY KEY (Pad_ID);
+ALTER TABLE TPLG_Pad
+    ADD CONSTRAINT C_Pad_PCBLib_FK FOREIGN KEY (Pad_PCBLib_ID) REFERENCES TPLG_PCB_Library(PCBLib_ID);
+ALTER TABLE TPLG_Pad
+    ADD CONSTRAINT C_Pad_Footprint_FK FOREIGN KEY (Pad_Footprint_ID) REFERENCES TPLG_Footprint(Footprint_ID);
+CREATE SEQUENCE S_TPLG_Pad INCREMENT BY 1 START WITH 1;
---
+++ seminar-6/insert.sql
@@ -0,0 +1,89 @@
+PROMPT Создаём библиотеку
+
+INSERT INTO TPLG_PCB_Library (PCBLib_ID, PCBLib_Name, PCBLib_Footprint_Count) 
+    VALUES (S_TPLG_PCB_Library.NEXTVAL, 'MyLib', 5);
+
+
+PROMPT Добавляем SMD резистор 0603
+
+INSERT INTO TPLG_Footprint (Footprint_PCBLib_ID, Footprint_ID, Footprint_Name, Footprint_Length, Footprint_Width, Footprint_Instalation, Footprint_Pad_Count)
+    VALUES ( (SELECT PCBLib_ID FROM TPLG_PCB_Library WHERE PCBLib_Name = 'MyLib'),S_TPLG_Footprint.NEXTVAL, 'R_0603', 2.8, 1, 'SMD', 2);
+
+INSERT INTO TPLG_Pad (Pad_PCBLib_ID, Pad_Footprint_ID, Pad_ID, Pad_Length, Pad_Width, Pad_Square, Pad_Hole_D, Pad_X, Pad_Y)
+    VALUES ((SELECT PCBLib_ID FROM TPLG_PCB_Library WHERE PCBLib_Name = 'MyLib'),
+        (SELECT Footprint_ID FROM TPLG_Footprint WHERE Footprint_Name = 'R_0603'),
+        S_TPLG_Pad.NEXTVAL, 1, 0.8, 'Round Rectangle', NULL, -0.8, 0);
+
+INSERT INTO TPLG_Pad (Pad_PCBLib_ID, Pad_Footprint_ID, Pad_ID, Pad_Length, Pad_Width, Pad_Square, Pad_Hole_D, Pad_X, Pad_Y)
+    VALUES ((SELECT PCBLib_ID FROM TPLG_PCB_Library WHERE PCBLib_Name = 'MyLib'),
+        (SELECT Footprint_ID FROM TPLG_Footprint WHERE Footprint_Name = 'R_0603'),
+        S_TPLG_Pad.NEXTVAL, 1, 0.8, 'Round Rectangle', NULL, 0.8, 0);
+
+
+PROMPT Добавляем SMD резистор 0806
+
+INSERT INTO TPLG_Footprint (Footprint_PCBLib_ID, Footprint_ID, Footprint_Name, Footprint_Length, Footprint_Width, Footprint_Instalation, Footprint_Pad_Count)
+    VALUES ( (SELECT PCBLib_ID FROM TPLG_PCB_Library WHERE PCBLib_Name = 'MyLib'),S_TPLG_Footprint.NEXTVAL, 'R_0805', 3.4, 1.6, 'SMD', 2);
+
+INSERT INTO TPLG_Pad (Pad_PCBLib_ID, Pad_Footprint_ID, Pad_ID, Pad_Length, Pad_Width, Pad_Square, Pad_Hole_D, Pad_X, Pad_Y)
+    VALUES ((SELECT PCBLib_ID FROM TPLG_PCB_Library WHERE PCBLib_Name = 'MyLib'),
+        (SELECT Footprint_ID FROM TPLG_Footprint WHERE Footprint_Name = 'R_0805'),
+        S_TPLG_Pad.NEXTVAL, 1.2, 1.3, 'Round Rectangle', NULL, -1, 0);
+
+INSERT INTO TPLG_Pad (Pad_PCBLib_ID, Pad_Footprint_ID, Pad_ID, Pad_Length, Pad_Width, Pad_Square, Pad_Hole_D, Pad_X, Pad_Y)
+    VALUES ((SELECT PCBLib_ID FROM TPLG_PCB_Library WHERE PCBLib_Name = 'MyLib'),
+        (SELECT Footprint_ID FROM TPLG_Footprint WHERE Footprint_Name = 'R_0805'),
+        S_TPLG_Pad.NEXTVAL, 1.2, 1.3, 'Round Rectangle', NULL, 1, 0);
+
+
+PROMPT Добавляем SMD конденсатор 0603
+
+INSERT INTO TPLG_Footprint (Footprint_PCBLib_ID, Footprint_ID, Footprint_Name, Footprint_Length, Footprint_Width, Footprint_Instalation, Footprint_Pad_Count)
+    VALUES ( (SELECT PCBLib_ID FROM TPLG_PCB_Library WHERE PCBLib_Name = 'MyLib'),S_TPLG_Footprint.NEXTVAL, 'C_0603', 2.8, 1, 'SMD', 2);
+
+INSERT INTO TPLG_Pad (Pad_PCBLib_ID, Pad_Footprint_ID, Pad_ID, Pad_Length, Pad_Width, Pad_Square, Pad_Hole_D, Pad_X, Pad_Y)
+    VALUES ((SELECT PCBLib_ID FROM TPLG_PCB_Library WHERE PCBLib_Name = 'MyLib'),
+        (SELECT Footprint_ID FROM TPLG_Footprint WHERE Footprint_Name = 'C_0603'),
+        S_TPLG_Pad.NEXTVAL, 1, 0.8, 'Round Rectangle', NULL, -0.8, 0);
+
+INSERT INTO TPLG_Pad (Pad_PCBLib_ID, Pad_Footprint_ID, Pad_ID, Pad_Length, Pad_Width, Pad_Square, Pad_Hole_D, Pad_X, Pad_Y)
+    VALUES ((SELECT PCBLib_ID FROM TPLG_PCB_Library WHERE PCBLib_Name = 'MyLib'),
+        (SELECT Footprint_ID FROM TPLG_Footprint WHERE Footprint_Name = 'C_0603'),
+        S_TPLG_Pad.NEXTVAL, 1, 0.8, 'Round Rectangle', NULL, 0.8, 0);
+
+
+PROMPT Добавляем SMD конденсатор 0806
+
+INSERT INTO TPLG_Footprint (Footprint_PCBLib_ID, Footprint_ID, Footprint_Name, Footprint_Length, Footprint_Width, Footprint_Instalation, Footprint_Pad_Count)
+    VALUES ( (SELECT PCBLib_ID FROM TPLG_PCB_Library WHERE PCBLib_Name = 'MyLib'),S_TPLG_Footprint.NEXTVAL, 'C_0805', 3.4, 1.6, 'SMD', 2);
+
+INSERT INTO TPLG_Pad (Pad_PCBLib_ID, Pad_Footprint_ID, Pad_ID, Pad_Length, Pad_Width, Pad_Square, Pad_Hole_D, Pad_X, Pad_Y)
+    VALUES ((SELECT PCBLib_ID FROM TPLG_PCB_Library WHERE PCBLib_Name = 'MyLib'),
+        (SELECT Footprint_ID FROM TPLG_Footprint WHERE Footprint_Name = 'C_0805'),
+        S_TPLG_Pad.NEXTVAL, 1.2, 1.3, 'Round Rectangle', NULL, -1, 0);
+
+INSERT INTO TPLG_Pad (Pad_PCBLib_ID, Pad_Footprint_ID, Pad_ID, Pad_Length, Pad_Width, Pad_Square, Pad_Hole_D, Pad_X, Pad_Y)
+    VALUES ((SELECT PCBLib_ID FROM TPLG_PCB_Library WHERE PCBLib_Name = 'MyLib'),
+        (SELECT Footprint_ID FROM TPLG_Footprint WHERE Footprint_Name = 'C_0805'),
+        S_TPLG_Pad.NEXTVAL, 1.2, 1.3, 'Round Rectangle', NULL, 1, 0);
+
+
+PROMPT Добавляем микросхемы в корпусе SOT23-3
+
+INSERT INTO TPLG_Footprint (Footprint_PCBLib_ID, Footprint_ID, Footprint_Name, Footprint_Length, Footprint_Width, Footprint_Instalation, Footprint_Pad_Count)
+    VALUES ( (SELECT PCBLib_ID FROM TPLG_PCB_Library WHERE PCBLib_Name = 'MyLib'),S_TPLG_Footprint.NEXTVAL, 'SOT23-3', 3.6, 1.2, 'SMD', 3);
+
+INSERT INTO TPLG_Pad (Pad_PCBLib_ID, Pad_Footprint_ID, Pad_ID, Pad_Length, Pad_Width, Pad_Square, Pad_Hole_D, Pad_X, Pad_Y)
+    VALUES ((SELECT PCBLib_ID FROM TPLG_PCB_Library WHERE PCBLib_Name = 'MyLib'),
+        (SELECT Footprint_ID FROM TPLG_Footprint WHERE Footprint_Name = 'SOT23-3'),
+        S_TPLG_Pad.NEXTVAL, 0.8, 0.5, 'Round Rectangle', NULL, -1.25, 0.95);
+
+INSERT INTO TPLG_Pad (Pad_PCBLib_ID, Pad_Footprint_ID, Pad_ID, Pad_Length, Pad_Width, Pad_Square, Pad_Hole_D, Pad_X, Pad_Y)
+    VALUES ((SELECT PCBLib_ID FROM TPLG_PCB_Library WHERE PCBLib_Name = 'MyLib'),
+        (SELECT Footprint_ID FROM TPLG_Footprint WHERE Footprint_Name = 'SOT23-3'),
+        S_TPLG_Pad.NEXTVAL, 0.8, 0.5, 'Round Rectangle', NULL, -1.25, -0.95);
+
+INSERT INTO TPLG_Pad (Pad_PCBLib_ID, Pad_Footprint_ID, Pad_ID, Pad_Length, Pad_Width, Pad_Square, Pad_Hole_D, Pad_X, Pad_Y)
+    VALUES ((SELECT PCBLib_ID FROM TPLG_PCB_Library WHERE PCBLib_Name = 'MyLib'),
+        (SELECT Footprint_ID FROM TPLG_Footprint WHERE Footprint_Name = 'SOT23-3'),
+        S_TPLG_Pad.NEXTVAL, 0.8, 0.5, 'Round Rectangle', NULL, 1.25, 0);
